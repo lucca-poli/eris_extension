2f7a68cda0c834fc41eb72d8f35ae6c188905f23
Luiz Carlos de Azevedo Santana <Luiz Carlos de Azevedo Santana>
2025-03-09 07:16:01 -0300

    Merge: button and selected conversation listener done

--- src/content.js
+++ src/content.js
@@ -1,7 +1,66 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'conversation_clicked') {
-        const { contactName } = message;
-        console.log(`User clicked on conversation with: ${contactName}`);
-        // You can perform additional actions here, such as storing the data or sending it to an external API
+window.addEventListener('message', (event) => {
+    //console.log("got a event:", event);
+    if (event.data.type === 'WPP_FULLY_READY') {
+        console.log('WA-JS is ready!');
+        clearInterval(event.data.intervalId);
+        console.log(document.body);
     }
 });
+
+class DomProcessor {
+    constructor() {
+        /** @type {string | null}
+         * @private
+         */
+        this.currentConversation = null;
+
+        this.updateSelectedConversation();
+    }
+
+    /** @private */
+    updateSelectedConversation() {
+        setInterval(() => {
+            const conversationPanel = document.getElementById("main");
+            if (conversationPanel !== null) {
+                const namePlaceholder = conversationPanel.querySelector("header")
+                    ?.children[1]?.firstElementChild?.firstElementChild?.firstElementChild?.firstElementChild;
+                /** @type {string | undefined} */
+                const contactNameRaw = (namePlaceholder?.firstElementChild?.alt !== undefined) ?
+                    namePlaceholder?.innerText + namePlaceholder?.firstElementChild?.alt :
+                    namePlaceholder?.innerText;
+                const contactName = contactNameRaw?.trim();
+
+                const isSingleContact = contactName !== "" && contactName !== undefined;
+                const isNewConversation = contactName !== this.currentConversation;
+                if (isSingleContact && isNewConversation) {
+                    this.currentConversation = contactName;
+                    this.attachInitAuditableConversationButton();
+                }
+            };
+        }, 1000);
+    }
+
+    /** @private */
+    attachInitAuditableConversationButton() {
+        const attachmentsDiv = document.getElementById("main").querySelector("footer")
+            ?.firstElementChild?.firstElementChild?.querySelector("span")?.firstElementChild?.firstElementChild;
+        console.log(attachmentsDiv);
+
+        const auditableButton = document.createElement("div");
+        auditableButton.className = "auditable-wpp-conversations-button";
+        auditableButton.style.width = "46px";
+        auditableButton.style.height = "52px";
+        auditableButton.style.display = "flex";
+        auditableButton.style.alignItems = "center";
+        auditableButton.style.justifyContent = "center";
+        auditableButton.style.cursor = "pointer";
+        auditableButton.style.transition = "opacity 0.2s ease-in-out";
+        auditableButton.innerText = "🔲"; // Replace with your own icon
+
+        auditableButton.addEventListener("click", () => console.log("Hello!"));
+
+        attachmentsDiv?.appendChild(auditableButton);
+    }
+}
+
+const domProcessorRepository = new DomProcessor();
--- src/wa-js.js
+++ src/wa-js.js
@@ -1,5 +1,17 @@
 import WPP from "@wppconnect/wa-js"
 
-const WPP = WPP;
+/** @type {typeof WPP} */
+const WhatsappLayer = window.WPP;
 
-console.log(WPP);
+function waitUserLogging(timeout = 30000) {
+    const startTime = Date.now();
+    const interval = setInterval(() => {
+        if (WhatsappLayer.isFullReady) {
+            //clearInterval(interval);
+            window.postMessage({ type: 'WPP_FULLY_READY', intervalId: interval }, '*');
+        } else if (Date.now() - startTime > timeout) {
+            //clearInterval(interval);
+            console.log("Login time has timed out!");
+        }
+    }, 1000);
+}
