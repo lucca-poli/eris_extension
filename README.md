57c755d230bf976103bc384b466dc608e050b161
Luiz Carlos de Azevedo Santana <Luiz Carlos de Azevedo Santana>
2025-03-29 10:32:29 -0300

    fix: improving messager

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # eris_extension
 
 # TODO
-- Passar pra TypeScript
 - Comunicador interno
+    - Implementar comunicar único que recebe estratégias de comunicação (window, chrome). Dependendo do from e to ele tem que saber que serviço ele vai usar e qual rota traçar se for mais de um. Acho que quem tem que guardar as rotas que faz é um método estático dos serviços, dai o messager pega as rotas de cada um e decide o caminho
     - Implementar response
-    - Implementar comunicar único que recebe estratégias de comunicação (window, chrome)
+- Passar pra TypeScript
 - Fazer logger e botar no comunicador
 - Implementar uma máquina de estados por chat privado. Por enquanto salvar o estado na RAM com um array de objetos no background. Só salvar se o estado for different de NORMAL_CHAT
--- src/utils/InternalMessager.js
+++ src/utils/InternalMessager.js
@@ -1,4 +1,26 @@
+import { AgentOptions } from "./types.js";
+
+export class InternalMessager {
+    // 1. Recebe os serviços de transmissão no constructor e o Owner. serviços é um array de InternalMessagerService
+    // O objeto message deve estabelecer suas próprias rotas na construção
+    /**
+     * @param {string} owner
+     * @param {Array} messagerServices
+    */
+    constructor(messagerServices, owner) {
+        this.owner = owner;
+    }
+    // 2. Send and receive messages: make the routing and decide the services per route
+}
+
 export class WindowMessager {
+    static getRoutes() {
+        const routes = {
+            //AgentOptions.CONTENT: AgentOptions.INJECTED,
+            //AgentOptions.INJECTED: AgentOptions.CONTENT,
+        };
+    }
+
     /**
      * @param {import("./types.js").InternalMessage} internalMessage
      * @returns {void}
